d65fd089497135f33fc7b2e1c8ab0f1a98af611b
FrancisGanio <92006134+FrancisGanio@users.noreply.github.com>
2022-04-11 10:59:21 +0800

    Try

---
+++ src/components/SignIn.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function SignIn() {
+    return ( 
+      <div>
+        <h1>My Todos</h1>
+        <div>
+          <h2>Title</h2>
+          <div>
+            <button>Delete</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default SignIn;
